233468a77f6e8c66130dd108bef521d657201ec5
Ivan Serebrennikov <admin@silberworks.com>
2026-01-02 05:04:26 +0300

    feat(version): set 1.0.1

--- esp8266_rewrite.sh
+++ esp8266_rewrite.sh
@@ -2,7 +2,7 @@
 screen -XS pts quit || true
 esptool.py --port /dev/ttyUSB0 erase_flash
 bash build_esp8266.sh
-esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 ESP8266_GENERIC-v1.26.1-PEPEUNIT-v1.0.0.bin
+esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 ESP8266_GENERIC-v1.26.1-PEPEUNIT-v1.0.1.bin
 
 echo "Run sync example files"
 ampy -p /dev/ttyUSB0 -b 115200 put ./example/ .
