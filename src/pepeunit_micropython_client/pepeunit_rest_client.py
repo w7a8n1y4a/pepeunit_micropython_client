87b6f020135b418306c7f6f8709d24d0fdb1291d
Ivan Serebrennikov <admin@silberworks.com>
2026-02-04 02:14:32 +0300

    feat(prc): alloc change

--- src/pepeunit_micropython_client/pepeunit_rest_client.py
+++ src/pepeunit_micropython_client/pepeunit_rest_client.py
@@ -9,7 +9,13 @@ class PepeunitRestClient:
     def __init__(self, settings):
         self.settings = settings
 
-    def _get_auth_headers(self):
+    def _get_auth_headers(self, with_json=False):
+        if with_json:
+            return {
+                'accept': 'application/json',
+                'x-auth-token': self.settings.PU_AUTH_TOKEN,
+                'content-type': 'application/json',
+            }
         return {
             'accept': 'application/json',
             'x-auth-token': self.settings.PU_AUTH_TOKEN,
@@ -24,6 +30,17 @@ class PepeunitRestClient:
             + self.settings.PU_API_ACTUAL_PREFIX
         )
 
+    def _raise_for_status(self, status, body=None):
+        if status < 400:
+            return
+        if body is None:
+            raise OSError("HTTP error {}".format(status))
+        try:
+            body_text = body.decode("utf-8") if isinstance(body, (bytes, bytearray)) else body
+        except Exception:
+            body_text = body
+        raise OSError("HTTP error {}: {}".format(status, body_text))
+
     async def _download_file(self, url, headers, file_path):
         status, _, _ = await request(
             "GET",
@@ -33,9 +50,7 @@ class PepeunitRestClient:
             bufsize=256,
             collect_headers=False,
         )
-        if status >= 400:
-            raise OSError("HTTP error {}".format(status))
-        gc.collect()
+        self._raise_for_status(status)
 
 
     async def download_update(self, file_path):
@@ -47,24 +62,20 @@ class PepeunitRestClient:
         url = self._get_base_url() + '/units/env/' + self.settings.unit_uuid
         headers = self._get_auth_headers()
         await self._download_file(url, headers, file_path)
-        gc.collect()
 
     async def download_schema(self, file_path):
         url = self._get_base_url() + '/units/get_current_schema/' + self.settings.unit_uuid
         headers = self._get_auth_headers()
 
         await self._download_file(url, headers, file_path)
-        gc.collect()
 
     async def set_state_storage(self, state):
         url = self._get_base_url() + '/units/set_state_storage/' + self.settings.unit_uuid
-        headers = self._get_auth_headers()
-        headers['content-type'] = 'application/json'
+        headers = self._get_auth_headers(with_json=True)
 
         payload = json.dumps({'state': state})
         status, _, _ = await request("POST", url, headers=headers, body=payload, max_body=2048)
-        if status >= 400:
-            raise OSError("HTTP error {}".format(status))
+        self._raise_for_status(status)
         gc.collect()
 
     async def get_state_storage(self):
@@ -72,9 +83,7 @@ class PepeunitRestClient:
         headers = self._get_auth_headers()
 
         status, _, body = await request("GET", url, headers=headers, max_body=4096)
-        if status >= 400:
-            raise OSError("HTTP error {}".format(status))
-        gc.collect()
+        self._raise_for_status(status, body)
 
         try:
             return body.decode("utf-8") if isinstance(body, (bytes, bytearray)) else body
@@ -82,24 +91,34 @@ class PepeunitRestClient:
             return body
 
     async def get_input_by_output(self, topic, limit=10, offset=0):
-        uuid = topic.split('/')[1]
+        first_sep = topic.find('/')
+        if first_sep < 0:
+            uuid = topic
+        else:
+            second_sep = topic.find('/', first_sep + 1)
+            uuid = topic[first_sep + 1:second_sep] if second_sep > first_sep else topic[first_sep + 1:]
 
         base_url = self._get_base_url() + '/unit_nodes'
         headers = self._get_auth_headers()
 
-        params = [
-            ('order_by_create_date', 'desc'),
-            ('output_uuid', uuid),
-            ('limit', str(limit)),
-            ('offset', str(offset)),
+        query_parts = [
+            'order_by_create_date=desc',
+            'output_uuid={}'.format(uuid),
+            'limit={}'.format(limit),
+            'offset={}'.format(offset),
         ]
-        query = '&'.join(['{}={}'.format(k, v) for (k, v) in params])
+        query = '&'.join(query_parts)
         url = base_url + '?' + query
 
         gc.collect()
-        status, _, body = await request("GET", url, headers=headers, max_body=16_000)
-        if status >= 400:
-            raise OSError("HTTP error {}".format(status))
+        status, _, body = await request(
+            "GET",
+            url,
+            headers=headers,
+            max_body=16_000,
+            collect_headers=False,
+        )
+        self._raise_for_status(status, body)
 
         gc.collect()
         data = json.loads(body)
@@ -113,24 +132,29 @@ class PepeunitRestClient:
         base_url = self._get_base_url() + '/units'
         headers = self._get_auth_headers()
 
-        params = [
-            ('is_include_output_unit_nodes', 'true'),
-            ('order_by_unit_name', 'asc'),
-            ('order_by_create_date', 'desc'),
-            ('order_by_last_update', 'desc'),
-            ('limit', str(limit)),
-            ('offset', str(offset)),
+        query_parts = [
+            'is_include_output_unit_nodes=true',
+            'order_by_unit_name=asc',
+            'order_by_create_date=desc',
+            'order_by_last_update=desc',
+            'limit={}'.format(limit),
+            'offset={}'.format(offset),
         ]
         for uuid in unit_node_uuids:
-            params.append(('unit_node_uuids', uuid))
+            query_parts.append('unit_node_uuids={}'.format(uuid))
 
-        query = '&'.join(['{}={}'.format(k, v) for (k, v) in params])
+        query = '&'.join(query_parts)
         url = base_url + '?' + query
 
         gc.collect()
-        status, _, body = await request("GET", url, headers=headers, max_body=32_000)
-        if status >= 400:
-            raise OSError("HTTP error {}".format(status))
+        status, _, body = await request(
+            "GET",
+            url,
+            headers=headers,
+            max_body=32_000,
+            collect_headers=False,
+        )
+        self._raise_for_status(status, body)
 
         gc.collect()
         data = json.loads(body)
